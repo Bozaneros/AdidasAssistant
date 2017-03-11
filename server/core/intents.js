271e122f70c510775a754e042e698c382c65f4cb
AMarquez94 <alejandromarquez94@gmail.com>
2017-03-11 06:25:16 +0100

    Bot saludando

---
+++ server/core/intents.js
@@ -0,0 +1,13 @@
+const random = array => { return array[Math.floor(Math.random() * array.length)] }
+const getGreetings = () => {
+    const answers = [
+        'Hello!',
+        'Hello! How are you? What do you need?',
+        'Hey, nice to see you.',
+        'Welcome back!',
+        'Hi, I\'m the Adidas Assistant. How can I help you?',
+        'Hey, what do you need?',
+    ];
+    return random(answers)
+}
+module.exports = getGreetings;
--- server/routes/bot.js
+++ server/routes/bot.js
@@ -6,6 +6,11 @@ const express = require('express');
 const router = express.Router();
 const config = require('../config/config');
 const builder = require('botbuilder');
+const recast = require('recastai');
+
+const getGreetings = require('../core/intents');
+
+const recastClient = new recast.Client(config.recast_dev);
 
 let connector = new builder.ChatConnector({
     appId: config.bot_id,
@@ -14,8 +19,25 @@ let connector = new builder.ChatConnector({
 
 router.post('/messages', connector.listen());
 
+const INTENTS = {
+    greetings: getGreetings
+};
+
 var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("hola");
+    recastClient.textRequest(session.message.text)
+        .then(res => {
+            const intent = res.intent();
+            console.log(intent);
+            const entity = res.get('trainers');
+            if(intent){
+                session.send(INTENTS[intent.slug](entity))
+            } else{
+                console.log("askfañsjfd");
+                session.send("hey");
+            }
+            //session.send(`Entity: ${entity.name}`);
+        })
+        .catch(() => session.send('I need some sleep right now... Talk to me later!'));
 });
 
 module.exports = router;
