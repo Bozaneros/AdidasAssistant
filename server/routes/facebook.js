66769aa7b253bfc908324d0169007797b6090341
AMarquez94 <alejandromarquez94@gmail.com>
2017-03-11 01:35:35 +0100

    Port changed to 8080

--- server/routes/facebook.js
+++ server/routes/facebook.js
@@ -4,10 +4,18 @@
 
 const express = require('express');
 const router = express.Router();
+const config = require('../config/config');
 
 /* GET api listing. */
 router.get('', (req, res) => {
-    res.status(200).json("Perfect");
+    if (req.query['hub.mode'] === 'subscribe' &&
+        req.query['hub.verify_token'] === config.verify_token) {
+        console.log("Validating webhook");
+        res.status(200).send(req.query['hub.challenge']);
+    } else {
+        console.error("Failed validation. Make sure the validation tokens match.");
+        res.sendStatus(403);
+    }
 });
 
 module.exports = router;
