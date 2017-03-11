3357af420679103f9819525918549efb2fe18836
Raul Piraces <raul.piraces@gmail.com>
2017-03-11 15:39:53 +0100

    Now procesing attachments 2

--- server/routes/facebook.js
+++ server/routes/facebook.js
@@ -143,64 +143,76 @@ function receivedMessage(event) {
 
 function processAttachment(senderID, messageAttachments){
     console.log(messageAttachments);
-    var exec = require('child_process').exec;
-    var cmd = 'python ../main.py ' + url;
-    var newCapture = new capture();
-    exec(cmd, function(error, stdout, stderr) {
-        console.log(error);
-        var firstLine = stdout.split('\n')[0];
-        var arr = firstLine.split(" ");
-        switch(arr[0]) {
-            case bb1302:
-                newCapture.code = "bb1302";
-                break;
-            case zxflux:
-                newCapture.code = "zxflux";
-                break;
-            case c77124:
-                newCapture.code = "c77124";
-                break;
-            case ba8278:
-                newCapture.code = "ba8278";
-                break;
-            case bb0008:
-                newCapture.code = "bb0008";
-                break;
-            case bb5477:
-                newCapture.code = "bb5477";
-                break;
-            case boost:
-                newCapture.code = "boost";
-                break;
-            case adidas:
-                newCapture.code = "adidas";
+    // Iterate over each entry - there may be multiple if batched
+    messageAttachments.forEach(function(attachment) {
+
+        switch (attachment.type) {
+            case "image":
+                var exec = require('child_process').exec;
+
+                var cmd = 'python ../main.py ' + attachment.payload.url;
+                var newCapture = new capture();
+                exec(cmd, function (error, stdout, stderr) {
+                    console.log(error);
+                    var firstLine = stdout.split('\n')[0];
+                    var arr = firstLine.split(" ");
+                    switch (arr[0]) {
+                        case bb1302:
+                            newCapture.code = "bb1302";
+                            break;
+                        case zxflux:
+                            newCapture.code = "zxflux";
+                            break;
+                        case c77124:
+                            newCapture.code = "c77124";
+                            break;
+                        case ba8278:
+                            newCapture.code = "ba8278";
+                            break;
+                        case bb0008:
+                            newCapture.code = "bb0008";
+                            break;
+                        case bb5477:
+                            newCapture.code = "bb5477";
+                            break;
+                        case boost:
+                            newCapture.code = "boost";
+                            break;
+                        case adidas:
+                            newCapture.code = "adidas";
+                            break;
+                        default:
+                            break;
+                    }
+                    newCapture.id = senderID;
+                    newCapture.name = "";
+                    newCapture.user = "";
+                    newCapture.score = parseFloat(arr[3].replace(')', ''));
+                    newUser.save(function (err, data) {
+                        if (err) {
+                            console.log(err);
+                        }
+                        shoe.findOne({code: newCapture.code}, function (err, data) {
+                            if (err) {
+                                //Error servidor
+                                response = {"error": true, "message": "Fetching error"};
+                                res.status(500).json(response);
+                            } else {
+                                var randBegin = randomBegin[Math.floor(Math.random() * randomBegin.length)];
+                                response = randBegin + "\"" + data.name + "\"" + ". " + data.description
+                                    + ". You have them for " + data.price + "$ at adidas.com";
+                                sendTextMessage(senderID, response);
+                            }
+                        })
+                    });
+                    console.log(stdout);
+                    console.log(stderr);
+                });
                 break;
             default:
+                sendTextMessage(senderID, "I don't know about this...");
                 break;
         }
-        newCapture.id = senderID;
-        newCapture.name = "";
-        newCapture.user = "";
-        newCapture.score = parseFloat(arr[3].replace(')',''));
-        newUser.save(function(err, data){
-            if(err){
-                console.log(err);
-            }
-            shoe.findOne({code:newCapture.code}, function(err, data){
-                if(err){
-                    //Error servidor
-                    response = {"error": true, "message": "Fetching error"};
-                    res.status(500).json(response);
-                } else {
-                    var randBegin = randomBegin[Math.floor(Math.random() * randomBegin.length)];
-                    response = randBegin + "\"" + data.name + "\"" + ". " + data.description
-                        + ". You have them for " + data.price + "$ at adidas.com";
-                    sendTextMessage(senderID, response);
-                }
-            })
-        });
-        console.log(stdout);
-        console.log(stderr);
     });
 
 }
